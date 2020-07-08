6f3454b6f25bf47ec64a3ffef04b93e1c6ef36b3
landry fokam <landrydejesus03@gmail.com>
2020-07-08 21:09:44 +0200

    Updates

---
+++ precache-manifest.941667a687d16a0789d9f2b6d2d5bab7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44cecdfa01154e815708d7378f3fb819",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fed42eb6d55f999e3f40",
+    "url": "/portfolio/static/css/main.8052addd.chunk.css"
+  },
+  {
+    "revision": "4263857b6b4a225170f1",
+    "url": "/portfolio/static/js/2.f7ecb67e.chunk.js"
+  },
+  {
+    "revision": "fed42eb6d55f999e3f40",
+    "url": "/portfolio/static/js/main.6f4daf3b.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
